60dcbf4d8318a72707cf32f9b86ac22ff07658c8
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-06 19:37:55 +0600

    updated code

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php require "header.php"; ?>
 <main class="dev-crud-main">
-    <h2 class="text-danger">Hello</h2>
+    <div class="container">
+        <div class="row mt-5">
+            <?php for ($i = 0; $i < 12; $i++) { ?>
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="card m-2">
+                        <img src="demo.jpg" class="card-img-top" alt="image">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                                the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 </main>
 <?php require "footer.php"; ?>
