92fa75a817cf4b99821d25bf7c84028eab64cf54
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-06 23:24:59 +0600

    single blog

---
+++ single-blog.php
@@ -0,0 +1,25 @@
+<?php require "header.php"; ?>
+<main class="dev-crud-main">
+    <div class="container">
+        <div class="row mt-5">
+
+            <div class="card m-2">
+                <img src="demo.jpg" class="card-img-top" alt="image">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae, voluptatem
+                        quis tempora debitis fuga error laudantium suscipit aut nihil, expedita inventore beatae
+                        repudiandae doloribus soluta cumque? Aspernatur sapiente neque, reiciendis possimus voluptatibus
+                        repellendus aperiam provident amet iste! Vero consequatur sit id nihil excepturi distinctio quo
+                        perferendis sunt mollitia tempora reiciendis, sed reprehenderit dignissimos ducimus nesciunt
+                        dolorum voluptatum quasi cumque saepe unde illum. Nemo at quae, veniam in facilis, voluptates
+                        magni iusto omnis adipisci laborum sunt reiciendis? Quod expedita vero, obcaecati exercitationem
+                        sed tempore explicabo! Nulla ipsam reiciendis doloremque enim recusandae eaque harum quidem
+                        assumenda, unde neque et sequi? Nulla, amet?</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</main>
+<?php require "footer.php"; ?>
