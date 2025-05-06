42e21f99c07483cbc834094801555670b1acaa7c
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-06 22:28:08 +0600

    login

--- login.php
+++ login.php
@@ -1,5 +1,21 @@
 <?php require "header.php"; ?>
 <main class="dev-crud-main">
-login
+    <div class="container my-5">
+
+        <h2 class="text-center">Login</h2>
+        
+        <form action="" class="w-50 mx-auto text-center">
+            <div class="mb-3 d-flex flex-row justify-content-center align-items-center">
+                <label for="username" class="form-label me-2">Username</label>
+                <input type="username" class="form-control" id="dev-crud-username" name="username" >
+            </div>
+            <div class="mb-3 d-flex flex-row justify-content-center align-items-center">
+                <label for="password" class="form-label me-2">Password</label>
+                <input type="password" class="form-control" id="dev-crud-password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
 </main>
 <?php require "footer.php"; ?>
