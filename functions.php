435a9cfb56999cd0cb5830b28a528091ea8b37b8
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-07 14:23:59 +0600

    updated code

--- functions.php
+++ functions.php
@@ -1,29 +1,53 @@
 <?php
-function dev_crud_register_new_user(){
-    if($_SERVER["REQUEST_METHOD"]== "POST" && isset($_POST["register_submit"])){
-        $username= $_POST["username"];
-        $password= $_POST["password"];
-        $hashed_password= password_hash($password, PASSWORD_DEFAULT);
+function dev_crud_register_new_user()
+{
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register_submit"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $confirm_password = $_POST["confirm_password"];
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
 
         //prepare insert query
         global $conn;
-        $stmt= $conn->prepare("INSERT INTO dc_users(username, password) VALUES(?, ?)");
-        $stmt->bind_param("ss", $username, $hashed_password);
-        //execute
-        if($stmt->execute()){
-            echo"user created successfully <br>";
+
+        //check if username already exists
+
+        $checkstmt = $conn->prepare("SELECT id from dc_users WHERE username= ?");
+        $checkstmt->bind_param("s", $username);
+        $checkstmt->execute();
+        $checkstmt->store_result();
+        if ($checkstmt->num_rows > 0) {
+            echo "username already exists!";
+        } else {
+
+            $stmt = $conn->prepare("INSERT INTO dc_users(username, password) VALUES(?, ?)");
+            $stmt->bind_param("ss", $username, $hashed_password);
+
+            //execute
+
+            if ($password === $confirm_password) {
+                if ($stmt->execute()) {
+                    echo "user created successfully <br>";
+                } else {
+                    echo "error creating user!";
+                }
+
+            } else {
+                echo "password didn't match!";
+            }
+
+
+            $stmt->close();
+
         }
 
-        else{
-            echo "Error: " . $stmt->error;
-            print_r("error!!!");
-        }
-        
-        
-        $stmt->close();
+
+
+        $checkstmt->close();
         $conn->close();
-    }
-    else{
+    } else {
         echo "error";
     }
 }
--- registration.php
+++ registration.php
@@ -7,15 +7,15 @@
         <form action="" method="POST" class="w-50 mx-auto text-center">
             <div class="mb-3 d-flex flex-row justify-content-center align-items-center">
                 <label for="username" class="form-label me-2">Username</label>
-                <input type="username" class="form-control" id="dev-crud-username" name="username">
+                <input type="username" class="form-control" id="dev-crud-username" name="username" required>
             </div>
             <div class="mb-3 d-flex flex-row justify-content-center align-items-center">
                 <label for="password" class="form-label me-2">Password</label>
-                <input type="password" class="form-control" id="dev-crud-password" name="password">
+                <input type="password" class="form-control" id="dev-crud-password" name="password" required>
             </div>
             <div class="mb-3 d-flex flex-row justify-content-center align-items-center">
                 <label for="confirm_password" class="form-label me-2">Confirm Password</label>
-                <input type="password" class="form-control" id="dev-crud-confirm-password" name="confirm_password">
+                <input type="password" class="form-control" id="dev-crud-confirm-password" name="confirm_password" required>
             </div>
             <button type="submit" id="dev-crud-register-submit" name="register_submit" class="btn btn-primary">Submit</button>
         </form>
