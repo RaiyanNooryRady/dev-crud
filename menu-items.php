9dafa0b9218c9f374524edabc30b17de0cf2bfa5
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-09 18:14:51 +0600

    menu items

--- menu-items.php
+++ menu-items.php
@@ -3,5 +3,7 @@
     <a href="#">Dashboard</a>
     <a href="#">Profile</a>
     <a href="#">Settings</a>
+    <a href="#">Users</a>
+    <a href="#">Posts</a>
     <a href="logout.php">Logout</a>
 </div>
