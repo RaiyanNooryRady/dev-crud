d606b4490c44d14cd54fd1e28fc4d36cad64d3e5
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-11 17:04:54 +0600

    users list

--- dashboard-users.php
+++ dashboard-users.php
@@ -16,8 +16,34 @@ if (!is_user_logged_in()) {
                 </div>
 
                 <div class="col-md-9 col-lg-10 col-12 p-4">
-                    <h1>Welcome, <?php echo $_SESSION['loggedin_user']; ?>!</h1>
-                    <p>This is your dashboard users content area.</p>
+                    <h1>Users List</h1>
+                    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addUserModal">Add New User</button>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>Profile Photo</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                            global $conn;
+                            $users= $conn->query("SELECT * FROM dc_users");
+                            foreach ($users as $user) {
+                                ?>
+                                <tr>
+                                    <td><?php echo $user['username']; ?></td>
+                                <td><img src="demo.jpg" class="dev-crud-user-photo img-fluid" alt="" srcset=""></td>
+                                <td><a href="dashboard-users.php?id=<?php echo $user['id']; ?>" class="btn btn-primary"><i class="bi bi-pencil-square"></i></a></td>
+                                <td><a href="dashboard-users.php?id=<?php echo $user['id']; ?>" class="btn btn-danger"><i class="bi bi-trash"></i></a></td>
+                            </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
