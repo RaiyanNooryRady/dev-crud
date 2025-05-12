bfc837dd6036e09850353c5b08c016a8288d4170
RaiyanNooryRady <raiyannooryrady@gmail.com>
2025-05-12 15:35:35 +0600

    posts created successfully

--- db-query.php
+++ db-query.php
@@ -6,9 +6,24 @@ username VARCHAR(50) NOT NULL UNIQUE,
 password VARCHAR(255) NOT NULL,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )";
-if($conn->query($create_users_table)){
+if ($conn->query($create_users_table)) {
     //echo "users table created successfully <br>";
     echo " ";
-}else{
+} else {
     echo "error!";
 }
+//sql to create posts table
+$create_posts_table = "CREATE TABLE IF NOT EXISTS dc_posts(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    featured_image VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+if ($conn->query($create_posts_table)) {
+    echo "posts table created successfully <br>";
+} else {
+    echo "error!";
+}
+
